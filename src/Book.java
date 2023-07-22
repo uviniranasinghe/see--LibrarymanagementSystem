a49ea61cb8a1669ace235e65a1fe62da42c89ed7
uviniranasinghe <uviniranasinghe21@gmail.com>
2023-07-22 10:45:08 +0530

    Book Class created

---
+++ src/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+    private int ISBN;
+    private String title;
+    private String category;
+    private String author;
+
+    public  int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
